ca417330285705506b1955ab83d1e2a9123c6348
Colin Alexander <chalexander@me.com>
2019-08-22 17:44:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reversed_array = [] 
-  array.each do |n|
-    reversed_array.push(n.reverse)
+  reversed_array = array.collect do |n|
+    n.reverse
   end
   return reversed_array.join(" ")
 end
